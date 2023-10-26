0c8c7d70e0d77e1a356c19c7e205f79cbb07d999
Illia <ilof@Illias-Mac-Studio.local>
2023-10-26 22:34:13 +0200

    Added logging for api calls, small refactoring

--- backend/shipments_manager/api/shipments/views.py
+++ backend/shipments_manager/api/shipments/views.py
@@ -22,31 +22,46 @@ class ShipmentsList(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        logger.error("Failed to create shipment")
+        logger.error(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ShipmentDetail(APIView):
-    def get_shipment(self, pk):
+    def get_shipment(self, pk: int):
         try:
             return Shipment.objects.get(id=pk)
         except Shipment.DoesNotExist:
             raise Http404
+
+    def update_shipment(self, request, pk, *, partial: bool = False) -> ShipmentSerializer:
+        shipment = self.get_shipment(pk)
+        serializer = ShipmentSerializer(shipment, data=request.data, partial=partial)
+        if serializer.is_valid():
+            serializer.save()
+            return serializer
+        logger.error("Failed to update shipment")
+        logger.error(serializer.errors)
+        return serializer
 
     def get(self, request, pk):
         shipment = self.get_shipment(pk)
         serializer = ShipmentSerializer(shipment)
         return Response(data=serializer.data)
 
-    def put(self, request, pk):
-        shipment = self.get_shipment(pk)
-        serializer = ShipmentSerializer(shipment, data=request.data, partial=True)
-        if serializer.is_valid():
-            logger.info(serializer)
-            serializer.save()
+    def put(self, request, pk: int):
+        serializer = self.update_shipment(request, pk)
+        if not serializer.errors:
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
+    def patch(self, request, pk: int):
+        serializer = self.update_shipment(request, pk, partial=True)
+        if not serializer.errors:
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk: int):
         shipment = self.get_shipment(pk)
         shipment.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
